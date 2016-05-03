07e9c2226d5235b6b18124fccd345956a85d4785
Nick Lyman and Nadiya Yeroshkina <nick@>
2016-05-03 16:52:59 -0700

    Trying to pass find BDD test for doctor specialty but gettina assertion error.

--- src/main/java/Specialty.java
+++ src/main/java/Specialty.java
@@ -4,12 +4,48 @@ import org.sql2o.*;
 
 public class Specialty {
   private int id;
-  private String specialty_name;
+  private String specialty;
   private int doctor_id;
 
 
-  public Specialty(String specialty_name, int doctor_id) {
-    this.specialty_name = specialty_name;
+  public Specialty(String specialty, int doctor_id) {
+    this.specialty = specialty;
     this.doctor_id = doctor_id;
   }
+
+  public String getName() {
+    return specialty;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Specialty> all() {
+   String sql = "SELECT id, specialty, doctor_id FROM specialties";
+   try(Connection con = DB.sql2o.open()) {
+     return con.createQuery(sql).executeAndFetch(Specialty.class);
+   }
+ }
+
+ public void save() {
+   try(Connection con = DB.sql2o.open()) {
+     String sql = "INSERT INTO specialties (specialty, doctor_id) VALUES (:specialty, :doctor_id)";
+     this.id = (int) con.createQuery(sql, true)
+       .addParameter("specialty", this.specialty)
+       .addParameter("doctor_id", this.doctor_id)
+       .executeUpdate()
+       .getKey();
+   }
+ }
+
+ public static Specialty find(int id) {
+   try(Connection con = DB.sql2o.open()) {
+     String sql = "SELECT * FROM specialties WHERE id=:id";
+     Specialty specialty = con.createQuery(sql)
+       .addParameter("id", id)
+       .executeAndFetchFirst(Specialty.class);
+     return specialty;
+   }
+ }
 }
--- src/test/java/SpecialtyTest.java
+++ src/test/java/SpecialtyTest.java
@@ -4,26 +4,53 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 
 public class SpecialtyTest {
-  // @Before
-  // public void setUp() {
-  //   DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/doctor_test", null, null);
-  // }
-  //
-  // @After
-  // public void tearDown() {
-  //   try(Connection con = DB.sql2o.open()) {
-  //     String deleteDoctorsQuery = "DELETE FROM doctors *;";
-  //     String deletePatientsQuery = "DELETE FROM patients *;";
-  //     String deleteSpecialtiesQuery = "DELETE FROM specialties *;";
-  //     con.createQuery(deleteDoctorsQuery).executeUpdate();
-  //     con.createQuery(deletePatientsQuery).executeUpdate();
-  //     con.createQuery(deleteSpecialtiesQuery).executeUpdate();
-  //   }
-  // }
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/doctor_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteDoctorsQuery = "DELETE FROM doctors *;";
+      String deletePatientsQuery = "DELETE FROM patients *;";
+      String deleteSpecialtiesQuery = "DELETE FROM specialties *;";
+      con.createQuery(deleteDoctorsQuery).executeUpdate();
+      con.createQuery(deletePatientsQuery).executeUpdate();
+      con.createQuery(deleteSpecialtiesQuery).executeUpdate();
+    }
+  }
 
   @Test
   public void specialty_instantiatesCorrectly_true() {
     Specialty testSpecialty = new Specialty("dentist", 1);
     assertEquals(true, testSpecialty instanceof Specialty);
+  }
+
+  @Test
+  public void getName_specialtyWithName_String(){
+    Specialty mySpecialty = new Specialty("dentist", 1);
+    assertEquals("dentist", mySpecialty.getName());
+  }
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Specialty.all().size(), 0);
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Specialty mySpecialty = new Specialty("dentist", 1);
+    mySpecialty.save();
+    Specialty savedSpecialty = Specialty.all().get(0);
+    assertEquals(mySpecialty.getId(), savedSpecialty.getId());
+  }
+
+  @Test
+  public void find_findsSpecialtyInDatabase_True() {
+    Specialty mySpecialty = new Specialty("dentist", 1);
+    mySpecialty.save();
+    Specialty savedSpecialty = Specialty.find(mySpecialty.getId());
+    assertTrue(mySpecialty.equals(savedSpecialty));
   }
 }
